4605f4ae9651573682d31f1e0672ab8a2e62d45a
Alphaharrius <alphaharrius@gmail.com>
2023-04-06 17:23:40 +0800

    Adding unique identifier to modes.

--- src/quantum.jl
+++ src/quantum.jl
@@ -6,12 +6,22 @@ using SparseArrays
 using ..Spaces
 
 struct Mode <: AbstractSubset{Mode}
+    group_name::String
+    indexes::Tuple
     base::Subset
     fermion_count::Int64
     space::AbstractSpace
 
-    Mode(base::Subset, fermion_count::Int64) = new(base, fermion_count, space_of(base))
+    Mode(group_name::String, indexes::Tuple, base::Subset, fermion_count::Int64) = new(group_name, indexes, base, fermion_count, space_of(base))
 end
+
+Base.:show(io::IO, mode::Mode) = print(io, "$(group_name(mode))$(string(indexes(mode)))")
+
+group_name(mode::Mode)::String = mode.group_name
+
+indexes(mode::Mode)::Tuple = mode.indexes
+
+Base.:(==)(a::Mode, b::Mode)::Bool = group_name(a) == group_name(b) && indexes(a) == indexes(b)
 
 struct FockSpace <: AbstractSpace{Subset{Subset{Mode}}}
     rep::Subset{Subset{Mode}}
@@ -22,14 +32,16 @@ struct FockSpace <: AbstractSpace{Subset{Subset{Mode}}}
         subsets,
         Dict(mode => order
              for (order, mode)
-             in enumerate([element for subset in representation(subsets) for element in representation(subset)])))
+             in enumerate([element for subset in rep(subsets) for element in rep(subset)])))
 
     FockSpace(subset::Subset{Mode}) = FockSpace(convert(Subset{Subset{Mode}}, subset))
 end
 
+Base.:show(io::IO, fock_space::FockSpace) = print(io, string(rep(flatten(rep(fock_space)))))
+
 dimension(fock_space::FockSpace)::Int64 = length(fock_space.ordering)
 
-Base.:(==)(a::FockSpace, b::FockSpace)::Bool = representation(a) == representation(b)
+Base.:(==)(a::FockSpace, b::FockSpace)::Bool = rep(a) == rep(b)
 
 Base.:convert(::Type{Subset}, source::FockSpace) = source.rep
 Base.:convert(::Type{Subset{Subset}}, source::FockSpace) = convert(Subset, source)
@@ -37,10 +49,14 @@ Base.:convert(::Type{Subset{Subset{Mode}}}, source::FockSpace) = convert(Subset,
 
 Base.:convert(::Type{FockSpace}, source::Subset{Mode}) = FockSpace(source)
 
-quantize(point::Point, fermion_count)::Mode = Mode(convert(Subset, point), fermion_count)
+quantize(group_name::String, indexes::Tuple, point::Point, fermion_count)::Mode = (
+    Mode(group_name, indexes, convert(Subset, point), fermion_count))
 
-function quantize(subset::Subset{T}, fermion_count::Int64)::Subset where {T <: AbstractSubset}
-    quantized::Vector = [quantize(element, fermion_count) for element in representation(subset)]
+quantize(group_name::String, subset::Subset, fermion_count::Int64)::Subset = (
+    quantize(group_name, tuple(), subset, fermion_count))
+
+function quantize(group_name::String, indexes::Tuple, subset::Subset{T}, fermion_count::Int64)::Subset where {T <: AbstractSubset}
+    quantized::Vector = [quantize(group_name, (indexes..., index), element, fermion_count) for (index, element) in enumerate(rep(subset))]
     return Subset(Set(quantized), space_of(quantized[1]))
 end
 
@@ -62,11 +78,11 @@ end
 
 function columns(fock_map::FockMap, subset::Subset{Mode})::FockMap
     in_space::FockSpace = FockSpace(subset)
-    rep::SparseMatrixCSC{ComplexF64, Int64} = spzeros(dimension(fock_map.out_space), dimension(in_space))
-    for mode in representation(subset)
-        rep[:, in_space.ordering[mode]] = representation(fock_map)[:, fock_map.out_space.ordering[mode]]
+    matrix::SparseMatrixCSC{ComplexF64, Int64} = spzeros(dimension(fock_map.out_space), dimension(in_space))
+    for mode in rep(subset)
+        matrix[:, in_space.ordering[mode]] = rep(fock_map)[:, fock_map.out_space.ordering[mode]]
     end
-    return FockMap(in_space, fock_map.out_space, rep)
+    return FockMap(in_space, fock_map.out_space, matrix)
 end
 
 Base.:convert(::Type{SparseMatrixCSC{ComplexF64, Int64}}, source::FockMap) = source.rep

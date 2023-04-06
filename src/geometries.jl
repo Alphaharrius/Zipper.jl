5ad8925888978a114b58b3e27cc494f36dd2f0a2
Alphaharrius <alphaharrius@gmail.com>
2023-04-06 17:22:41 +0800

    Added brillouin zone.

--- src/geometries.jl
+++ src/geometries.jl
@@ -4,8 +4,35 @@ module Geometries
 
 using ..Spaces
 
-radius(region::Subset, center::Point)::Float64 = maximum(distance(center, point) for point in representation(region))
+radius(region::Subset, center::Point)::Float64 = maximum(distance(center, point) for point in rep(region))
 
-export radius
+struct Crystal
+    unit_cell::Subset{Point}
+    shape::Vector{Int64}
+end
+
+resize(crystal::Crystal, shape::Vector{Int64})::Crystal = Crystal(crystal.unit_cell, shape)
+
+mesh(shape::Vector{Int64})::Matrix{Int64} = hcat([collect(tup) for tup in collect(Iterators.product([0:(d - 1) for d in shape]...))[:]]...)
+
+function points(crystal::Crystal)::Subset{Point}
+    real_space::RealSpace = space_of(crystal.unit_cell)
+    crystal_mesh::Matrix{Int64} = mesh(crystal.shape)
+    mesh_points::Array{Point} = [Point(pos, real_space) for pos in eachcol(crystal_mesh)]
+    points::Set{Point} = Set{Point}([mesh_point + point for mesh_point in mesh_points for point in rep(crystal.unit_cell)])
+    return Subset(points, real_space)
+end
+
+function brillouin_zone(crystal::Crystal)::Subset{Point}
+    momentum_space::MomentumSpace = convert(MomentumSpace, space_of(crystal.unit_cell))
+    crystal_mesh::Matrix{Int64} = mesh(crystal.shape)
+    tiled_shape::Matrix{Int64} = hcat([crystal.shape for i in 1:size(crystal_mesh, 2)]...)
+    recentered_mesh::Matrix{Float64} = (crystal_mesh - tiled_shape / 2) ./ tiled_shape
+    points::Set{Point} = Set{Point}([Point(pos, momentum_space) for pos in eachcol(recentered_mesh)])
+    return Subset(points, momentum_space)
+end
+
+export Crystal
+export radius, resize, mesh, points, brillouin_zone
 
 end

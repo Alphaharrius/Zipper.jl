7aba2074c3e1ea6a8cba934a20ae7a73a9d26c4b
Alphaharrius <alphaharrius@gmail.com>
2023-04-11 00:06:06 +0800

    Using foreach.

--- src/physical.jl
+++ src/physical.jl
@@ -15,12 +15,7 @@ function bloch(bonds::Set{Bond}, k::Point)::FockMap
     all_modes::Set{Mode} = Set([mode for bond in bonds for mode in bond.modes])
     fock_space::FockSpace = FockSpace(Subset(all_modes))
     fock_dict::Dict{Tuple{Mode, Mode}, ComplexF64} = Dict()
-    for bond in bonds
-        if !haskey(fock_dict, bond.modes)
-            fock_dict[bond.modes] = 0im
-        end
-        fock_dict[bond.modes] += fourier_coef(k, bond.offset) * bond.strength
-    end
+    foreach(bond -> fock_dict[bond.modes] = get(fock_dict, bond.modes, 0im) + fourier_coef(k, bond.offset) * bond.strength, bonds)
     upper_triangular::FockMap = FockMap(fock_space, fock_space, fock_dict)
     return upper_triangular + dagger(upper_triangular)
 end

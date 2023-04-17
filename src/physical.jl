959eb6960afc6209185a82556dfdbcf02c323f99
Alphaharrius <alphaharrius@gmail.com>
2023-04-17 20:24:34 +0800

    Removing old version of bloch hamiltonian APIs.

--- src/physical.jl
+++ src/physical.jl
@@ -6,21 +6,6 @@ module Physical
 
 using ..Spaces, ..Quantum, ..Geometries
 
-struct Bond
-    modes::Tuple{Mode, Mode}
-    offset::Point
-    strength::Number
-end
-
-function bloch(bonds::Set{Bond}, k::Point, crystal::Crystal)::FockMap
-    all_modes::Set{Mode} = Set([setattr(mode, :offset => k) for bond in bonds for mode in bond.modes])
-    fock_space::FockSpace = FockSpace(Subset(all_modes))
-    fock_dict::Dict{Tuple{Mode, Mode}, ComplexF64} = Dict()
-    foreach(bond -> fock_dict[bond.modes] = get(fock_dict, bond.modes, 0im) + fourier_coef(k, bond.offset, vol(crystal)) * bond.strength, bonds)
-    upper_triangular::FockMap = FockMap(fock_space, fock_space, fock_dict)
-    return upper_triangular + dagger(upper_triangular)
-end
-
 function bondmap(bonds::Vector{Pair{Tuple{Mode, Mode}, ComplexF64}})::FockMap
     modes::Set{Mode} = Set([mode for bond in bonds for mode in bond.first])
     fockspace::FockSpace = FockSpace(Subset(modes))

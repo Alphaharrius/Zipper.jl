98d76218f19a8e64372dbe2e578f406e0067b35b
Alphaharrius <alphaharrius@gmail.com>
2023-05-11 22:52:00 +0800

    Renaming methods.

--- src/zer.jl
+++ src/zer.jl
@@ -10,27 +10,27 @@ export DistillRegion
 export frozenisometries, frozenselectionbythreshold, frozenselectionbycount
 
 function frozenselectionbythreshold(threshold::Float64)
-    function frozenfocks(𝐶ᵣ::FockMap)::Dict{Symbol, FockMap}
+    function frozenfockmaps(𝐶ᵣ::FockMap)::Dict{Symbol, FockMap}
         spectrum, 𝑈ᵣ::FockMap = eigh(𝐶ᵣ)
         filledmodes::Subset{Mode} = Subset(map(p -> p.first, filter(p -> p.second < threshold, spectrum)))
         emptymodes::Subset{Mode} = Subset(map(p -> p.first, filter(p -> p.second > 1.0 - threshold, spectrum)))
         return Dict(:filled => columns(𝑈ᵣ, FockSpace(filledmodes)), :empty => columns(𝑈ᵣ, FockSpace(emptymodes)))
     end
-    return frozenfocks
+    return frozenfockmaps
 end
 
 function frozenselectionbycount(count::Integer)
-    function frozenfocks(𝐶ᵣ::FockMap)::Dict{Symbol, FockMap}
+    function frozenfockmaps(𝐶ᵣ::FockMap)::Dict{Symbol, FockMap}
         𝑈ᵣ::FockMap = eigvecsh(𝐶ᵣ)
-        modes::Subset{Mode} = orderedmodes(𝑈ᵣ.inspace)
-        return Dict(:filled => columns(𝑈ᵣ, FockSpace(modes[1:count])), :empty => columns(𝑈ᵣ, FockSpace(modes[(end - count):end])))
+        modes::Vector{Mode} = [orderedmodes(𝑈ᵣ.inspace)...]
+        return Dict(:filled => columns(𝑈ᵣ, FockSpace(Subset(modes[1:count]))), :empty => columns(𝑈ᵣ, FockSpace(Subset(modes[(end - count + 1):end]))))
     end
-    return frozenfocks
+    return frozenfockmaps
 end
 
-function frozenisometries(crystal::Crystal, correlations::FockMap, restrictedfock::FockSpace;
+function frozenisometries(correlations::FockMap, restrictedfock::FockSpace;
                           selectionstrategy = frozenselectionbythreshold(1e-3))::Dict{Symbol, FockMap}
-    𝐹ₖ::FockMap = fourier(brillouinzone(crystal), restrictedfock) / sqrt(vol(crystal))
+    𝐹ₖ::FockMap = fourier(correlations.inspace, restrictedfock) / sqrt(subspacecount(correlations.inspace))
     𝐶ᵣ::FockMap = 𝐹ₖ' * correlations * 𝐹ₖ
     return selectionstrategy(𝐶ᵣ)
 end

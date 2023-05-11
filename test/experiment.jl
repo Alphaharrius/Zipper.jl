f7cb550ea585bcab5d8d326e783d7d2bb01cb527
Alphaharrius <alphaharrius@gmail.com>
2023-05-11 22:53:07 +0800

    Updated test cases.

--- test/experiment.jl
+++ test/experiment.jl
@@ -3,11 +3,11 @@ include("../src/geometries.jl")
 include("../src/quantum.jl")
 include("../src/physical.jl")
 include("../src/plotting.jl")
-include("../src/zer.jl")
 include("../src/transformations.jl")
+include("../src/zer.jl")
 
 using PlotlyJS, LinearAlgebra, OrderedCollections, SparseArrays, ColorTypes, SmithNormalForm
-using ..Spaces, ..Geometries, ..Quantum, ..Physical, ..Zer, ..Transformations
+using ..Spaces, ..Geometries, ..Quantum, ..Physical, ..Transformations, ..Zer
 using ..Plotting
 
 triangular = RealSpace([sqrt(3)/2 -1/2; 0. 1.]')
@@ -16,12 +16,6 @@ kspace = convert(MomentumSpace, triangular)
 unitcell = union(Point([1/3, 2/3], triangular), Point([2/3, 1/3], triangular))
 crystal = Crystal(unitcell, [32, 32])
 modes::Subset{Mode} = quantize(:pos, unitcell, 1)
-fullfock::FockSpace = crystalfock(modes, crystal)
-
-scale = Scale([2 0; 0 2])
-recipient = Recipient(fullfock, :crystal => crystal)
-blockmap = scale * recipient
-newcrystal = scale * crystal
 
 tₙ = ComplexF64(-1.)
 m0, m1 = members(modes)
@@ -31,32 +25,47 @@ bonds::FockMap = bondmap([
     (m0, setattr(m1, :offset => Point([0, 1], triangular))) => tₙ])
 
 𝐻::FockMap = hamiltonian(crystal, bonds)
-visualize(𝐻, title="Hamiltonian", rowrange=1:64, colrange=1:64)
-filled::FockMap = groundstates(𝐻)
+𝐶::FockMap = groundstatecorrelation(𝐻)
 
-𝐶::FockMap = idmap(filled.outspace, filled.outspace) - filled * filled'
+blocked = blocking(:scale => Scale([2. 0.; 0. 2.]), :correlations => 𝐶, :crystal => crystal)
+blockedcorrelations::FockMap = blocked[:correlations]
+
+visualize(𝐻, title="Hamiltonian", rowrange=1:64, colrange=1:64)
 visualize(𝐶, title="Correlation", rowrange=1:64, colrange=1:64)
-blockedcorrelation = blockmap * 𝐶 * blockmap'
-visualize(blockedcorrelation, title="Correlation", rowrange=1:64, colrange=1:64)
+visualize(blockedcorrelations, title="Correlation", rowrange=1:64, colrange=1:64)
+
+newcrystal = blocked[:crystal]
 
 crystalpoints::Subset{Point} = latticepoints(newcrystal)
-newmodes::Subset{Mode} = removeattr(rep(blockedcorrelation.outspace)[1], :offset)
+newmodes::Subset{Mode} = quantize(:pos, newcrystal.unitcell, 1, group=ModeGroup(transformed, "scaled"))
 physicalmodes::Subset{Mode} = spanoffset(newmodes, crystalpoints)
-restrictedregion::Subset{Mode} = filter(circularfilter(origin(euclidean(RealSpace, 2)), 1.0), physicalmodes)
+restrictedregion::Subset{Mode} = filter(circularfilter(origin(euclidean(RealSpace, 2)), 2.0), physicalmodes)
 restrictedfock::FockSpace = FockSpace(restrictedregion)
 
-restrictfourier::FockMap = fourier(blockedcorrelation.inspace, restrictedfock) / sqrt(vol(newcrystal))
+isometries = frozenisometries(blocked[:correlations], restrictedfock, selectionstrategy=frozenselectionbycount(3))
+emptyprojector::FockMap = isometries[:empty] * isometries[:empty]'
+filledprojector::FockMap = isometries[:filled] * isometries[:filled]'
+projector::FockMap = emptyprojector - filledprojector
+pvs, pu = eigh(projector)
+plot(scatter(y=map(p -> p.second, pvs), mode="markers"))
+visualize(pu)
+cmode::Mode = orderedmodes(pu.inspace)[4]
+cmr::FockMap = columns(pu, FockSpace(Subset([cmode])))
+cspec = columnspec(cmr)
+visualize(cspec)
+
+restrictfourier::FockMap = fourier(blockedcorrelations.outspace, restrictedfock) / sqrt(vol(newcrystal))
 visualize(restrictfourier, rowrange=1:32)
-restrictedcorrelation::FockMap = restrictfourier' * blockedcorrelation * restrictfourier
-visualize(restrictedcorrelation)
-crvs = eigvalsh(restrictedcorrelation)
+restrictedcorrelations::FockMap = restrictfourier' * blockedcorrelations * restrictfourier
+visualize(restrictedcorrelations)
+crvs = eigvalsh(restrictedcorrelations)
 plot(scatter(y=map(p -> p.second, crvs), mode="markers"))
 
-restrictedunitary::FockMap = eigvecsh(restrictedcorrelation)
+restrictedunitary::FockMap = eigvecsh(restrictedcorrelations)
 visualize(restrictedunitary)
 
-emode1::Mode = orderedmodes(restrictedunitary.inspace)[4]
-emode2::Mode = orderedmodes(restrictedunitary.inspace)[5]
+emode1::Mode = orderedmodes(restrictedunitary.inspace)[11]
+emode2::Mode = orderedmodes(restrictedunitary.inspace)[12]
 mr1::FockMap = columns(restrictedunitary, FockSpace(Subset([emode1])))
 mr2::FockMap = columns(restrictedunitary, FockSpace(Subset([emode2])))
 mr = FockMap(mr1.outspace, mr1.inspace, rep(mr1) + 1im * rep(mr2))

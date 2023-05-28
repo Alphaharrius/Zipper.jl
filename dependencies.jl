89462e8630fda05afcbfd8f3ed84f6347160995d
Alphaharrius <alphaharrius@gmail.com>
2023-05-28 21:59:30 +0800

    Adding new dependencies for matrix operations.

--- dependencies.jl
+++ dependencies.jl
@@ -3,5 +3,6 @@ using Pkg
 Pkg.add("PlotlyJS")
 Pkg.add("OrderedCollections")
 Pkg.add("ColorTypes")
+Pkg.add("Compat")
 Pkg.Registry.add(RegistrySpec(url="https://github.com/wildart/BoffinStuff.git"))
 Pkg.add("SmithNormalForm")

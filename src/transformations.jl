b5833b26cf3ab39b8d40d987f2cf7ad116df7d9c
Alphaharrius <alphaharrius@gmail.com>
2023-10-02 19:32:29 +0800

    Allow adding custom eigenfunctions.

--- src/transformations.jl
+++ src/transformations.jl
@@ -151,6 +151,14 @@ function AffineTransform(
     return AffineTransform(localspace, shiftvector, transformmatrix, eigenfunctiontable, eigenvaluehashdenominator, antiunitary)
 end
 
+function addeigenfunction!(transform::AffineTransform; eigenvalue::Number, eigenfunction::BasisFunction)
+    eigenvaluekey::Tuple = hashablecomplex(eigenvalue |> Complex, transform.eigenvaluehashdenominator)
+    trialphase::Complex = relativephase(transform * eigenfunction, eigenfunction)
+    hashablecomplex(trialphase |> Complex, transform.eigenvaluehashdenominator) == eigenvaluekey || error("The eigenvalue is not associated with the eigenfunction!")
+    transform.eigenfunctions[eigenvaluekey] = eigenfunction
+end
+export addeigenfunction!
+
 pointgrouptransform(
     pointgroupmatrix::Matrix;
     dimension::Integer = pointgroupmatrix |> size |> first,

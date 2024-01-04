99f077ee05f35514d3d6024c5ed57d8ab007db25
Alphaharrius <alphaharrius@gmail.com>
2024-01-04 15:21:22 +0800

    Updated blocking to use `CrystalFockMap`.

--- src/lib/quantumtransformations.jl
+++ src/lib/quantumtransformations.jl
@@ -4,38 +4,29 @@ function Base.:*(scale::Scale, crystalfock::FockSpace{Crystal})::FockMap
     unscaledblockedregion::Subset{Offset} = (scale |> inv) * scaledcrystal.unitcell
     bz::Subset{Momentum} = crystal |> brillouinzone
     basismodes::Subset{Mode} = crystalfock |> rep |> first
-    scaledbz::Subset{Momentum} = scaledcrystal |> brillouinzone
 
     momentummappings::Base.Generator = (basispoint(scale * p) => p for p in bz)
-    mappingpartitions::Dict{Point, Vector{Point}} = foldl(momentummappings; init=Dict{Point, Vector{Point}}()) do d, (k, v)
-        mergewith!(append!, d, LittleDict(k => [v]))
-    end
 
-    ksubsets::Dict{Momentum, Subset{Mode}} = crystalfock |> crystalsubsets
-    scaledfock::FockSpace = ((ksubsets[k] for k in mappingpartitions[kscaled]) |> subsetunion |> FockSpace
-        for kscaled in scaledbz) |> fockspaceunion
-
-    # TODO: Use of latticeoff requires revisit, since latticeoff only truncates the decimals.
     unscaledblockedlatticeoffsets::Subset{Offset} = Subset(pbc(crystal, p) |> latticeoff for p in unscaledblockedregion)
     unscaledblockedunitcellfock::FockSpace = spanoffset(basismodes, unscaledblockedlatticeoffsets)
 
-    restrictedfourier::FockMap = fourier(bz, unscaledblockedunitcellfock)
     volumeratio::Number = (crystal |> vol) / (scaledcrystal |> vol)
-    permutedfourier::FockMap = Zipper.permute(restrictedfourier, outspace=scaledfock) / sqrt(volumeratio)
 
-    function repackfourierblocks(source::FockMap, kscaled::Momentum, partition::Subset{Mode})::FockMap
-        partitionrows::FockMap = rows(source, partition |> FockSpace)
-        inspace::FockSpace = (Subset(setattr(mode, :offset => kscaled, :pos => scale * convert(Point, mode))
-            for mode in partitionrows.inspace |> orderedmodes)
-            |> FockSpace)
-        return FockMap(partitionrows.outspace, inspace, partitionrows |> rep)
+    crystalfocksubspaces::Dict{Momentum, FockSpace} = crystalfock|>crystalsubspaces|>Dict
+    restrictedfourier::FockMap = fourier(bz, unscaledblockedunitcellfock)'
+    blocks::Dict = Dict()
+
+    scaledksubspaces::Dict{Momentum, FockSpace} = Dict()
+    for (scaledk, k) in momentummappings
+        kfourier::FockMap = columns(restrictedfourier, crystalfocksubspaces[k]) / sqrt(volumeratio)
+        if !haskey(scaledksubspaces, scaledk)
+            scaledksubspaces[scaledk] = FockSpace(
+                setattr(mode, :offset=>scaledk, :pos=>(scale * getpos(mode))) for mode in kfourier|>getoutspace)
+        end
+        blocks[(scaledk, k)] = FockMap(scaledksubspaces[scaledk], kfourier|>getinspace, kfourier|>rep)
     end
 
-    repackedblocks::Base.Generator = (
-        repackfourierblocks(permutedfourier, kscaled, partition)
-        for (kscaled, partition) in Iterators.zip(scaledbz, scaledfock |> rep))
-    blocking::FockMap = directsum(repackedblocks)
-    return FockMap(blocking, inspace=FockSpace(blocking |> getinspace, reflected=scaledcrystal), outspace=crystalfock)'
+    return CrystalFockMap(scaledcrystal, crystal, blocks)
 end
 
 function Base.:*(transformation::AffineTransform, regionfock::FockSpace{Region})::FockMap

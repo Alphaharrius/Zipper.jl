3db165fb3a1e39a2d021b8fca344041adc803da8
Alphaharrius <alphaharrius@gmail.com>
2024-02-13 02:46:34 +0800

    `arguments...` gives a `Tuple` by default.

--- src/lib/memorization.jl
+++ src/lib/memorization.jl
@@ -4,13 +4,12 @@ function memorization(f::Function)
     fsymbol = Symbol(f)
     haskey(MEMORIZED, fsymbol) || (MEMORIZED[fsymbol] = Dict())
     return function (arguments...)
-        key = Tuple(arguments...)
-        if haskey(MEMORIZED[fsymbol], key)
-            @info "Using memorized value for $(f) with arguments $(key)"
-            return MEMORIZED[fsymbol][key]
+        if haskey(MEMORIZED[fsymbol], arguments)
+            @info "Using memorized value for $(f) with arguments $(arguments)"
+            return MEMORIZED[fsymbol][arguments]
         else
             result = f(arguments...)
-            MEMORIZED[fsymbol][key] = result
+            MEMORIZED[fsymbol][arguments] = result
             return result
         end
     end

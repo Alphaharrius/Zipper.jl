3048b83045c291d177acd36776b632f313e3be43
Alphaharrius <alphaharrius@gmail.com>
2024-03-13 13:41:01 +0800

    Reorganizing test cases.

--- test/tutorial.jl
+++
@@ -1,9 +0,0 @@
-B = [1/8 3*sqrt(3)/8 9/8 3*sqrt(3)/8; -sqrt(3)/8 -5/8 -sqrt(3)/8 3/8; 3/8 sqrt(3)/8 -5/8 sqrt(3)/8; -3*sqrt(3)/8 9/8 -3*sqrt(3)/8 1/8]'
-B |> eigvecs
-A = [cos(π/3) -sin(π/3); sin(π/3) cos(π/3)]
-
-A[:, 1] * A[:, 1]'
-
-function tmatcontractrule(dimension::Integer, order::Integer)
-
-end
